f56e068bba8cfb4daa8a8df0b2a30dad6f00c739
jamminP <parkjaemin08@gmail.com>
2024-05-24 22:28:04 +0900

    Revert "Merge pull request #15 from mseony/feature/login"
    This reverts commit 414dfb0ee8d51b9b9628af6aee94d8e48b6b9364, reversing
    changes made to 2e3b4046780fc7cf2a820e728afb90caf3eff029.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,35 +1,10 @@
 import React from "react";
-import loginImage from './google_logo.png'; // 컴포넌트 파일과 같은 폴더에 있는 이미지 파일
 
-function Login() {
-    return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            fontFamily: 'Arial, sans-serif'
-        }}>
-            <h1 style={{ marginBottom: '20px', fontSize: '60px', fontWeight: 'bold' }}>Sign in to Favicon</h1>
-            <button style={{
-                display: 'flex',
-                alignItems: 'center',
-                padding: '10px 20px',
-                border: '1px solid #ddd',
-                borderRadius: '5px',
-                backgroundColor: 'white',
-                cursor: 'pointer',
-                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
-            }}>
-                <img 
-                    src={loginImage} 
-                    alt="Google Logo" 
-                    style={{ width: '40px', height: '40px', marginRight: '10px' }}
-                />
-                <span style={{ fontSize: '30px', color: '#555' }}>Continue with Google</span>
-            </button>
-        </div>
+function Login(){
+    
+    return(
+
+        <div>login</div>
     );
 }
 
